c530723668e984588d1923bae1abdb4ced05185c
hemareact <hemaarivu2010@gmail.com>
2022-07-25 23:11:42 +0530

    init

--- react2cls/src/App.js
+++ react2cls/src/App.js
@@ -4,138 +4,83 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Carousel from 'react-bootstrap/Carousel';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Carousel from 'react-bootstrap/Carousel';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 
-function app() {
-    return (
+function BasicExample() {
+  return (
+    
       <Container>
-      <Navbar bg='secondary' expand="lg">
-      
-        <Navbar.Brand href="#home" text-align="center">POWER OPTIMIZER AND EFFICIENCY SYSTEM</Navbar.Brand>
+      <Navbar bg="warning" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#link">Link</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Service</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-              About
+                Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Contact</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-        
-    </Navbar>
-    
      
-         <Row>
-        <Col >
-        <Carousel>
+    </Navbar>
+    <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://gumlet.assettype.com/freepressjournal/2022-01/55cc2294-d5c7-4e12-be2f-5bdf005afe2f/power_plant_4349830_960_720.webp?format=webp&w=480&dpr=2.6"
+          src="holder.js/800x400?text=First slide&bg=373940"
           alt="First slide"
         />
         <Carousel.Caption>
-          
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://assets.thehansindia.com/h-upload/2020/05/04/966656-thermal-power-plants.webp"
+          src="holder.js/800x400?text=Second slide&bg=282c34"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-         
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://media.gettyimages.com/photos/thermal-power-plant-picture-id1131689219?s=612x612"
+          src="holder.js/800x400?text=Third slide&bg=20232a"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-         
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
-    </Carousel>   
-    </Col>
-           <Col> <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" />
-      <Card.Body>
-        <Card.Title>INTRODUCTION</Card.Title>
-        <Card.Text>
-          It is a system which are used in the load side demand.Every system has seperate
-          voltage ceritia.In this project we have to find out where the voltage get drop
-          and comprise that.  
-        </Card.Text>
-        <Button variant="primary">More Details</Button>
-      </Card.Body>
-    </Card>
-    </Col>
+    </Carousel>
+    <Row>
+        <Col bg="dark" width="250px">1 of 3</Col>
+        <Col bg="success">2 of 3</Col>
+        <Col bg="warning">3 of 3</Col>
       </Row>
-      <Row> 
-        <div className="bg-secondary border p-5 text-center"> Some of the services which are considered in POES are given below </div> 
-    </Row>
-      <Row>
-        <Col>
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-        <Card.Title>System model considered</Card.Title>
-        <Card.Text>
-        The model system used , where a primary load of 157kwh/d, 22kW peak is connected to the AC bus.  
-        </Card.Text>
-        <Button variant="primary">More Details</Button>
-      </Card.Body>
-    </Card>
-        </Col>
-        <Col>
-        <Card style={{ width: '18rem' }}>
-      
-      <Card.Body>
-        <Card.Title>Service</Card.Title>
-        <Card.Text>
-        You need a service provider with extensive industry knowledge and acumen.  
-   
-        </Card.Text>
-        <Button variant="primary">More Details</Button>
-      </Card.Body>
-      </Card>
-        </Col>
-     
-      <Col>
-        <Card style={{ width: '18rem' }}>
-      
-      <Card.Body>
-        <Card.Title>Inputs and assumptions of system model</Card.Title>
-        <Card.Text>
-        Where there was a peak of 8.2kW at the early hours  ... 
-        </Card.Text>
-        <Button variant="primary">More Details</Button>
-      </Card.Body>
-      </Card>
-        </Col>
-        </Row>
-        <Row> 
-        <div className="bg-secondary border text=center"> Email-poessre@gmail.com </div> 
-    </Row>
-      </Container> 
-          
-     
+    
+   </Container>
   );
-}  
-   
-export default app;
+}
+
+export default BasicExample;
